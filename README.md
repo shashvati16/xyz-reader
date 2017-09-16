dc88d89c24ca687c31acee90bcf062b2b42292af
shashvati16 <shashvati16@users.noreply.github.com>
2017-09-15 20:11:06 -0400

    refactor: reorganized the project
    modified the paths,packages,etc

---
+++ README.md
@@ -0,0 +1,27 @@
+# XYZ Reader 
+A simple RSS feed reader app featuring  featuring topics and banners which implements elements of Material Design.
+
+## UI Mocks
+
+![screen 1](https://user-images.githubusercontent.com/23104247/30245300-8891afb0-95a2-11e7-84a1-dbb1196e70f4.PNG)
+![screen 2](https://user-images.githubusercontent.com/23104247/30245304-a5a28688-95a2-11e7-827d-03e16e67c785.PNG)
+![screen 3](https://user-images.githubusercontent.com/23104247/30245201-fd0aba06-959f-11e7-92e1-2edfabbbf375.PNG)
+
+
+
+## License
+```
+Copyright (C) 2017 Shashvati Dash - XYZReader App Project for Udacity Android Nanodegree
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ```
---
+++ app/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -0,0 +1,36 @@
+package com.example.xyzreader.ui;
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class DynamicHeightNetworkImageView extends NetworkImageView {
+    private float mAspectRatio = (2/3);
+
+    public DynamicHeightNetworkImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        int threeTwoHeight = (int) (measuredWidth/ mAspectRatio);
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight,MeasureSpec.EXACTLY);
+        setMeasuredDimension(measuredWidth,threeTwoHeightSpec);
+
+    }
+}
